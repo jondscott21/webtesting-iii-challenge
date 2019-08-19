36aa61c3da9d24692b7c0cbeb91aa7745e44bc60
Jonathan Scott <jonathan.scott@gmail.com>
2019-08-19 15:37:17 -0700

    mvp finished

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+    it('matches snapshot', () => {
+      const tree = renderer.create(<Controls />); 
+  
+
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    it('shows unlocked and open as a default state', () => {
+        const { getByText, queryByText } = render(<Controls />)
+        getByText(/lock gate/i)
+        getByText(/close gate/i)
+    })
+  });
